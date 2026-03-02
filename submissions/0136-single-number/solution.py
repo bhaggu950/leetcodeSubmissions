943940a66ab24abf005fdff481429c34193c7f8c
Bhargav Patel <52254787+bhaggu950@users.noreply.github.com>
2026-03-02 05:16:34 +0000

    [LeetCode Sync] Runtime - 3 ms (61.72%), Memory - 21.1 MB (60.52%)

---
+++ submissions/0136-single-number/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        # count_dict = {}
+        # for element in nums:
+        #     if element in list(count_dict.keys()):
+        #         count_dict[element] = count_dict[element]+1
+        #     else:
+        #         count_dict[element]=1
+        
+        # for key,value in count_dict.items():
+        #     if value==1:
+        #         return key
+        xoor = 0
+        for element in nums:
+            xoor^=element
+            
+        return xoor
