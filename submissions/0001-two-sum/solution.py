09a3cad34c68dc3c58f018a3f449ba0b52f6f320
Bhargav Patel <52254787+bhaggu950@users.noreply.github.com>
2026-03-05 03:19:13 +0000

    [LeetCode Sync] Runtime - 3 ms (52.78%), Memory - 20.8 MB (8.06%)

--- submissions/0001-two-sum/solution.py
+++ submissions/0001-two-sum/solution.py
@@ -1,11 +1,25 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        indices = {}
-
-        for index,current_number in enumerate(nums):
-            complement = target-current_number
-            if complement in indices.keys():
-                return [index,indices[complement]]
-            indices[current_number] = index
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        # for i in range(len(nums)-1):
+        #     remaining = target-nums[i]
+        #     if remaining in nums[i+1:]:
+        #         return [i, nums.index(remaining, i+1)]
+                        
+        # return [-1,-1]
         
-        return None
+        nums_with_index = [(num,idx) for idx,num in enumerate(nums)]
+        nums_with_index.sort(key=lambda x:x[0])
+        # nums.sort()
+        
+        left, right = 0, len(nums)-1
+        while left<right:
+            total = nums_with_index[left][0]+nums_with_index[right][0]
+            if total==target:
+                return [nums_with_index[left][1], nums_with_index[right][1]]
+            elif total<target:
+                left+=1
+            else:
+                right-=1
+                
+        return [-1,-1]
+        
