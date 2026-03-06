56db7b974f9feb3961465c09b3c67a0b284566a0
Bhargav Patel <52254787+bhaggu950@users.noreply.github.com>
2026-03-06 14:24:55 +0000

    [LeetCode Sync] Runtime - 4 ms (71.22%), Memory - 21 MB (97.56%)

---
+++ submissions/0169-majority-element/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def majorityElement(self, nums: list[int]) -> int:
+        nums_set = set(nums)
+        
+        for element in nums_set:
+            if nums.count(element) > int(len(nums)/2):
+                return element
+        
