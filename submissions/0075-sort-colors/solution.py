6ee3ce31e4b4127798da6bbd043ad6eefa18f281
Bhargav Patel <52254787+bhaggu950@users.noreply.github.com>
2026-03-06 02:55:05 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (87.00%)

---
+++ submissions/0075-sort-colors/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortColors(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            min, min_idx = nums[i], i
+            for j in range(i, len(nums)):
+                if nums[j]<min:
+                    min, min_idx = nums[j], j
+                
+            nums[i],nums[min_idx] = nums[min_idx], nums[i]
