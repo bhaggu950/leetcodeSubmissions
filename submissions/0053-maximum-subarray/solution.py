78e5dc544d7ad283f90c494709f251e9b192ba2c
Bhargav Patel <52254787+bhaggu950@users.noreply.github.com>
2026-03-09 03:37:10 +0000

    [LeetCode Sync] Runtime - 17 ms (97.80%), Memory - 31.3 MB (90.76%)

---
+++ submissions/0053-maximum-subarray/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxSubArray(self, nums: list[int]) -> int:
+        maximum = float('-inf')
+        n = len(nums)
+        total = 0
+        for element in nums:
+            total+=element
+            if total>maximum:
+                maximum = total
+            if total<0:
+                total = 0
+                    
+        return maximum
+        
