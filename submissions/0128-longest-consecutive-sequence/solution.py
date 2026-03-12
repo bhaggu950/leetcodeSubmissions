7bb50b5b5f9a3607c2f7d7dd8127d293eaca1f71
Bhargav Patel <52254787+bhaggu950@users.noreply.github.com>
2026-03-12 05:25:19 +0000

    [LeetCode Sync] Runtime - 32 ms (99.20%), Memory - 36.5 MB (83.79%)

---
+++ submissions/0128-longest-consecutive-sequence/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestConsecutive(self, nums: list[int]) -> int:
+        if len(nums)==0:
+            return 0
+        nums_set = sorted(set(nums))
+        current = nums_set[0]
+        maxLength = 1
+        currentLength = 1
+        for i in range(1,len(nums_set)):
+            element = nums_set[i]
+            if element == current+1:
+                currentLength+=1
+            else:
+                maxLength = max(maxLength, currentLength)
+                currentLength=1
+            current = element
+                
+        return max(maxLength, currentLength)
+            
