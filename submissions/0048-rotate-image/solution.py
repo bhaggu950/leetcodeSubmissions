bfc83751e37e562fe2957348095216f8cd99fea9
Bhargav Patel <52254787+bhaggu950@users.noreply.github.com>
2026-03-17 07:06:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (59.06%)

---
+++ submissions/0048-rotate-image/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rotate(self, matrix: list[list[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix[0])
+        i=0
+        while i<n-1:
+            for j in range(i+1,n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            i+=1
+        
+        for i in range(n):
+            matrix[i] = matrix[i][::-1]
+        
