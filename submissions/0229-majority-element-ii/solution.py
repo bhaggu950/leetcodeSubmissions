2676e551f0f5afc741e255684b2af47acb3eea80
Bhargav Patel <52254787+bhaggu950@users.noreply.github.com>
2026-04-08 07:04:33 +0000

    [LeetCode Sync] Runtime - 4 ms (81.69%), Memory - 23.2 MB (51.12%)

---
+++ submissions/0229-majority-element-ii/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def majorityElement(self, nums: list[int]) -> list[int]:
+        count_dict = {}
+        n = len(nums)
+        for element in nums:
+            count_dict[element] = count_dict.get(element, 0)+1
+        
+        result = []
+        for key,value in count_dict.items():
+            if value>(n/3):
+                result.append(key)
+        
+        return result
